3502dfab9d1172135f343203d3d8939f44d28e30
Zaden Yip <hyfrencn@gmail.com>
2026-06-09 01:34:52 +0800

    add base code for PG and Redis clients

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,13 @@
 package main
+import (
+	"github.com/zadenyip/enlangmemo-server/internal/config"
+	"github.com/zadenyip/enlangmemo-server/internal/infra/pg"
+	"github.com/zadenyip/enlangmemo-server/internal/infra/redisclient"
+)
 
 func main() {
+	config := config.Load()
+	db := pg.NewClient(config.DatabaseURL)
+	rdb := redisclient.NewClient(config.RedisURL)
 
 }
