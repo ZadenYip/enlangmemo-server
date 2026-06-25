eebb974d3b3f78cfdb638c1e76c7778fdc66a0be
Zaden Yip <hyfrencn@gmail.com>
2026-06-26 00:55:11 +0800

    fix: wrong data type for 'name' column in users table

--- docker/pg-init-scripts/init.sql
+++ docker/pg-init-scripts/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS oauth_clients (
 
 CREATE TABLE IF NOT EXISTS users (
     id uuid PRIMARY KEY DEFAULT uuidv7(),
-    name varying(16) NOT NULL UNIQUE,
+    name character varying(16) NOT NULL UNIQUE,
     -- 密码本身32字符以内，但以 argon2id hash 方式存
     password_hash text NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now()
